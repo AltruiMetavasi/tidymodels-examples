4cc98c481e0698e8b2807a208179fed856fe3375
R. Dimas Bagas Herlambang <bagasbgy@gmail.com>
2019-05-23 08:53:03 +0700

    added cross-validation example

--- R/data-clean.R
+++ R/data-clean.R
@@ -6,83 +6,35 @@ rm(list = ls())
 # import libs
 library(lubridate)
 library(snakecase)
+library(readxl)
 library(tidyverse)
 
 # import dataset ----------------------------------------------------------
 
 # import
-data_raw <- read_delim("data-raw/bank-full.csv", delim = ";")
+data_raw <- read_excel("data-raw/WA_Fn-UseC_-HR-Employee-Attrition.xlsx")
 
 # quick check
-# glimpse(data_raw)
-
-# filter-out "unknown" ----------------------------------------------------
-
-# check "unknown" data
-# data_raw %>%
-#   select_if(is.character) %>%
-#   mutate_all(as.factor) %>%
-#   summary()
-
-# filter-out abnormal "unknown"
-data_raw <- data_raw %>%
-  filter(
-    job != "unknown",
-    education != "unknown",
-    contact != "unknown",
-  )
-
-# check "unknown" data
-# data_raw %>%
-#   filter(previous != 0) %>%
-#   pull(poutcome) %>%
-#   table()
-
-# data_raw %>%
-#   filter(pdays != -1) %>%
-#   pull(poutcome) %>%
-#   table()
-
-# filter-out abnormal "unknown"
-data_raw <- data_raw %>%
-  filter(
-    !(previous != 0 & poutcome == "unknown"),
-    !(pdays != -1 & poutcome == "unknown")
-  )
-
-# quick check
-# glimpse(data_raw)
-
-# filter-out outliers -----------------------------------------------------
-
-# select eligible numeric columns
-num_cols <- data_raw %>%
-  select_if(is.numeric) %>%
-  select(-day) %>%
-  colnames()
-
-# filter-out outliers
-data_raw <- data_raw %>%
-  mutate_at(num_cols, ~ ifelse(abs(. - mean(.)) > 3 * sd(.), NA, .)) %>%
-  drop_na()
-
-# quick check
-# glimpse(data_raw)
+glimpse(data_raw)
 
 # readjust factors --------------------------------------------------------
+
+# readjust all column names
+data_raw <- data_raw %>%
+  rename_all(~ to_snake_case(.))
 
 # readjust all nominal columns
 data_raw <- data_raw %>%
   mutate_if(is.character, ~ to_snake_case(.))
 
 # quick check
-# glimpse(data_raw)
+glimpse(data_raw)
 
 # finalize ----------------------------------------------------------------
 
 # readjust columns
 data_clean <- data_raw %>%
-  select(everything(), response = y)
+  select(attrition, everything())
 
 # export ------------------------------------------------------------------
 
